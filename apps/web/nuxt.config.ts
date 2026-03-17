a6938cabe222f0b4f0aa9d452881cfd54322c6f2
subrina.tai <subrina8080@outlook.com>
2026-03-17 11:23:29 +0800

    chore(koda): initial monorepo scaffold

---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'shadcn-nuxt',
+  ],
+
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui',
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3100/api',
+    },
+  },
+
+  typescript: {
+    strict: true,
+  },
+})
