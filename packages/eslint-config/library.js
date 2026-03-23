48d7557b1bd9bc8153882221d869abad20b01531
williamkhoo <william_khoo@outlook.com>
2026-03-23 12:15:49 +0800

    fix: add eslint-config and typescript-config

---
+++ packages/eslint-config/library.js
@@ -0,0 +1,43 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ["eslint:recommended", "prettier", "turbo",'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  globals: {
+    // React: true,
+    // JSX: true,
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+    '.eslintrc.js',
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },  
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
+};
