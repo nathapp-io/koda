e413d9d8e52d3460d0a8697b31c36a32f6d1e24b
William Khoo <william_khoo@outlook.com>
2026-03-28 00:34:17 +0800

    fix(web): allow require imports in spec files via eslint override

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: ['@nathapp/eslint-config/vue'],
+  overrides: [
+    {
+      files: ['*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+      },
+    },
+  ],
 };
